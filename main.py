82e39bf2ccab64a9daa351b39c88f25d27404796
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-01 10:51:00 +0300

    ex-3 in progress

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 try:
-    list_of_numbers = []
-    while True:
-        num = input("Введіть число: ")
-        if num == "":
-            break
-        list_of_numbers.append(int(num))
 
-    number = int(input("Введіть число, кількість появ якого необхідно порахувати: "))
 
-    count = 0
-    for n in list_of_numbers:
-        if n == num:
-            count += 1
-
-    print("Кількість разів, коли число {} присутнє в списку: {}".format(
-        num, count))
 except Exception as e:
     print(e)
