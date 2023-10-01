d4360b6e1d60af5efade25d6d74da13d139ea979
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-01 11:05:02 +0300

    ex-3 in progress

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 try:
+    list_of_numbers = []
+    while True:
+        number = input("Введіть число: ")
+        if number == "":
+            break
+        list_of_numbers.append(int(number))
 
+    sum_of_numbers = 0
+    for number in list_of_numbers:
+        sum_of_numbers += number
+
+    average = sum_of_numbers / len(list_of_numbers)
+
+    print("Сума всіх елементів списку: {}".format(sum_of_numbers))
+    print("Середньоарифметичне всіх елементів списку: {}".format(average))
 
 except Exception as e:
     print(e)
